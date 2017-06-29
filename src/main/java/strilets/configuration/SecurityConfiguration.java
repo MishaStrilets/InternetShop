8c9aa041c2ce031c97c06df54f7684b153a7887d
Misha Strilets <mishastrilets@gmail.com>
2017-06-29 23:43:30 +0300

    add security

--- src/main/java/strilets/configuration/SecurityConfiguration.java
+++ src/main/java/strilets/configuration/SecurityConfiguration.java
@@ -1,19 +1,32 @@
 package strilets.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-				.authorizeRequests().antMatchers("/console/**").permitAll();
-
-		httpSecurity.csrf().disable();
-		httpSecurity.headers().frameOptions().disable();
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+			throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("12345")
+				.roles("ADMIN");
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.antMatchers("/")
+				.permitAll()
+				.antMatchers("/admin", "/admin/goods", "/admin/goods/{id}", "/admin/goods/new", "/admin/goods/edit/{id}",
+						"/admin/goods/delete/{id}").access("hasRole('ROLE_ADMIN')")
+				.and().formLogin().and().logout().permitAll()
+				.logoutUrl("/logout");
+	}
 }
--- src/main/java/strilets/controllers/GoodsController.java
+++ src/main/java/strilets/controllers/GoodsController.java
@@ -1,8 +1,15 @@
 package strilets.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import strilets.model.Goods;
 import strilets.services.GoodsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,40 +26,71 @@ public class GoodsController {
 		this.goodsService = goodsService;
 	}
 
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
 	@RequestMapping(value = "/goods", method = RequestMethod.GET)
 	public String list(Model model) {
 		model.addAttribute("goods", goodsService.listAllGoods());
 		return "goods";
 	}
 
-	@RequestMapping("goods/{id}")
+	@RequestMapping("/goods/{id}")
 	public String showGoods(@PathVariable Integer id, Model model) {
 		model.addAttribute("goods", goodsService.getGoodsById(id));
-		return "goodsshow";
+		return "goods_show";
 	}
 
-	@RequestMapping("goods/edit/{id}")
+	@RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
+	public String admin() {
+		return "admin_goods";
+	}
+
+	@RequestMapping(value = { "/logout" }, method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request,
+			HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext()
+				.getAuthentication();
+		if (auth != null)
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		return "index";
+	}
+
+	@RequestMapping(value = "/admin/goods", method = RequestMethod.GET)
+	public String adminGoods(Model model) {
+		model.addAttribute("goods", goodsService.listAllGoods());
+		return "admin_goods";
+	}
+
+	@RequestMapping("/admin/goods/{id}")
+	public String adminShowGoods(@PathVariable Integer id, Model model) {
+		model.addAttribute("goods", goodsService.getGoodsById(id));
+		return "admin_goods_show";
+	}
+
+	@RequestMapping("/admin/goods/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
 		model.addAttribute("goods", goodsService.getGoodsById(id));
-		return "goodsform";
+		return "admin_goods_form";
 	}
 
-	@RequestMapping("goods/new")
+	@RequestMapping("/admin/goods/new")
 	public String newGoods(Model model) {
 		model.addAttribute("goods", new Goods());
-		return "goodsform";
+		return "admin_goods_form";
 	}
 
-	@RequestMapping(value = "goods", method = RequestMethod.POST)
+	@RequestMapping(value = "/admin/goods", method = RequestMethod.POST)
 	public String saveGoods(Goods goods) {
 		goodsService.saveGoods(goods);
-		return "redirect:/goods/" + goods.getId();
+		return "redirect:/admin/goods/" + goods.getId();
 	}
 
-	@RequestMapping("goods/delete/{id}")
+	@RequestMapping("/admin/goods/delete/{id}")
 	public String delete(@PathVariable Integer id) {
 		goodsService.deleteGoods(id);
-		return "redirect:/goods";
+		return "redirect:/admin/goods";
 	}
-
 }
